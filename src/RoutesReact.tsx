942c38116a88c6420b065a1954cfdedd528633df
Cassio Izumi <izumicassio@gmail.com>
2023-04-18 09:04:25 -0300

    Added new list page

--- src/RoutesReact.tsx
+++ src/RoutesReact.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ListNotes from "./pages/ListNotes";
 
 function RoutesReact() {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="list-notes" element={<h1>HOME 1</h1>} />
+                <Route path="list-notes" element={<ListNotes/>} />
                 <Route path="about" element={<h1>ABOUT 123</h1>} />
                 <Route path="*" element={<h1>NotFound 404</h1>} />
             </Routes>
