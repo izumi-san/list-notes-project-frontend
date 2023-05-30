1d6f2c52575945bd1dabd40e0ead9ab61d856b36
Cassio Izumi <izumicassio@gmail.com>
2023-05-30 10:54:16 -0300

    Testing HashRouter to fix gh-pages

--- src/RoutesReact.tsx
+++ src/RoutesReact.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ListNotes from "./pages/ListNotes";
 
 function RoutesReact() {
 
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="list-notes" element={<ListNotes/>} />
                 <Route path="about" element={<h1>ABOUT 123</h1>} />
                 <Route path="*" element={<h1>NotFound 404</h1>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
